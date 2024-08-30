9e3b22756db4a79ed2a1e8ac59c31d5d6c76de9f
Light#0001 <152773601+y8done@users.noreply.github.com>
2024-08-30 14:31:33 +0530

    Sign up and login is done

--- app/src/test/java/com/example/womenprotection/ExampleUnitTest.java
+++ app/src/test/java/com/example/womenprotection/ExampleUnitTest.java
@@ -1,17 +1,14 @@
 package com.example.womenprotection;
 
-import org.junit.Test;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
